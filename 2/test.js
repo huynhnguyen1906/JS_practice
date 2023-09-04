78aadcd393d76cf356257eb96d664069963fdcec
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2023-09-04 10:25:19 +0900

    test check

--- 2/test.js
+++ 2/test.js
@@ -1,8 +1,2 @@
-var number = 7;
-var messenger1 = "chao buoi toi";
-var messenger2 = "chung ta cung nhau hoc js nao";
-
-var a = 1
-var b = 2
-var c = 3
-var d = 4
+var number = 20;
+document.write(number+2);
