4d08c5471d203cc67ba6b5c330c8d48504325164
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2023-09-23 13:14:29 +0900

    re-push form data

--- form/confirm.js
+++ form/confirm.js
@@ -1,11 +1,12 @@
 function init() {
     var confirmFirstName = document.getElementById('confirm_name');
-    confirmFirstName.innerHTML = sessionStorage.firstName + ' ' + sessionStorage.lastName;
     var confirmEmail = document.getElementById('confirm_email');
-    confirmEmail.innerHTML = sessionStorage.email;
     var confirmPhone = document.getElementById('confirm_phone');
-    confirmPhone.innerHTML = sessionStorage.phone;
     var confirmAge = document.getElementById('confirm_age');
+    
+    confirmFirstName.innerHTML = sessionStorage.firstName + ' ' + sessionStorage.lastName;
+    confirmEmail.innerHTML = sessionStorage.email;
+    confirmPhone.innerHTML = sessionStorage.phone;
     confirmAge.innerHTML = sessionStorage.age;
 }
 window.onload = init;
