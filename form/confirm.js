2044833ed54b7c1bff7d57108875eb9e778b0761
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2023-09-25 02:08:08 +0900

    use if else and switch case to fill and prefill data between

--- form/confirm.js
+++ form/confirm.js
@@ -3,11 +3,38 @@ function loadData() {
     var confirmEmail = document.getElementById('confirm_email');
     var confirmPhone = document.getElementById('confirm_phone');
     var confirmAge = document.getElementById('confirm_age');
+    var confirmOccupation = document.getElementById('confirm_occupation');
     
-    confirmFirstName.innerHTML = sessionStorage.firstName + ' ' + sessionStorage.lastName;
-    confirmEmail.innerHTML = sessionStorage.email;
-    confirmPhone.innerHTML = sessionStorage.phone;
-    confirmAge.innerHTML = sessionStorage.age;
+    if (sessionStorage.education === undefined) {
+        confirmOccupation.innerHTML = "You did not specify an occupation"
+    } else {
+        confirmOccupation.innerHTML = sessionStorage.education;
+    }
+
+    if (sessionStorage.firstName === "" && sessionStorage.lastName === "") {
+        confirmFirstName.innerHTML = "You did not specify a name";
+    } else {
+        confirmFirstName.innerHTML = sessionStorage.firstName + ' ' + sessionStorage.lastName;
+    }
+
+    if (sessionStorage.email === "") {
+        confirmEmail.innerHTML = "You did not specify an email";
+    } else {
+        confirmEmail.innerHTML = sessionStorage.email;
+    }
+
+    if (sessionStorage.phone === "") {
+        confirmPhone.innerHTML = "You did not specify a phone number";
+    } else {
+        confirmPhone.innerHTML = sessionStorage.phone;
+    }
+
+    if (sessionStorage.age === "") {
+        confirmAge.innerHTML = "You did not specify an age";
+    } else {
+        confirmAge.innerHTML = sessionStorage.age;
+    }
+
 }
 function init() {
     loadData();
@@ -15,6 +42,6 @@ function init() {
     cancelButton.onclick = function() {
         window.location = 'register.html';
     }
-    
+
 }
 window.onload = init;
--- form/register.js
+++ form/register.js
@@ -9,6 +9,24 @@ function saveData() {
     sessionStorage.age = age;
     sessionStorage.firstName = firstName;
     sessionStorage.lastName = lastName;
+    var highschool = document.getElementById('highschool').checked;
+    var university = document.getElementById('university').checked;
+    var working = document.getElementById('working').checked;
+    var retired = document.getElementById('retired').checked;
+    if (highschool === true) {
+        sessionStorage.education = "High School";
+    }
+    if (university === true) {
+        sessionStorage.education = "University";
+    }
+    if (working === true) {
+        sessionStorage.education = "Working";
+    }
+    if (retired === true) {
+        sessionStorage.education = "Retired";
+    }
+
+
 }
 function prefillData() {
     if (sessionStorage.firstName != undefined) {
@@ -26,6 +44,31 @@ function prefillData() {
     if (sessionStorage.age != undefined) {
         document.getElementById('age').value = sessionStorage.age;
     }
+    
+    if (sessionStorage.education === "highschool") {
+        document.getElementById('highschool').checked = true;
+    } else if (sessionStorage.education === "University") {
+        document.getElementById('university').checked = true;
+    } else if (sessionStorage.education === "Working") {
+        document.getElementById('working').checked = true;
+    } else  if (sessionStorage.education === "Retired") {
+        document.getElementById('retired').checked = true;
+    }
+    //truong trường hợp không sử dụng if else, mà sử dụng switch case
+    // switch (sessionStorage.education) {
+    //     case "High School":
+    //         document.getElementById('highschool').checked = true;
+    //         break;
+    //     case "University":
+    //         document.getElementById('university').checked = true;
+    //         break;
+    //     case "Working":
+    //         document.getElementById('working').checked = true;
+    //         break;
+    //     case "Retired":
+    //         document.getElementById('retired').checked = true;
+    //         break;
+    // }
 }
 function init() {
     var regForm = document.getElementById('registerForm');
