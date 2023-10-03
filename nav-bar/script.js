11d8287d13e6b4571115fe227396d11bb80ecfa5
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2023-10-03 19:54:23 +0900

    test import video from youtube

--- nav-bar/script.js
+++ nav-bar/script.js
@@ -1,20 +1,17 @@
 const menuItems = document.querySelectorAll('.menu-item');
 
-        window.addEventListener('scroll', () => {
-            // Lặp qua từng section và kiểm tra xem nó đã đủ cận đỉnh trình duyệt hay chưa
-            for (let i = 0; i < menuItems.length; i++) {
-                const section = document.getElementById(`section${i + 1}`);
-                const rect = section.getBoundingClientRect();
+    window.addEventListener('scroll', () => {
+    for (let i = 0; i < menuItems.length; i++) {
+        const section = document.getElementById(`section${i + 1}`);
+        const rect = section.getBoundingClientRect();
 
-                // Nếu phần trên của section hiện ra ở trên trình duyệt
-                if (rect.top >= 0 && rect.top <= 50) {
-                    // Xóa hiệu ứng "active" ở tất cả các mục
-                    menuItems.forEach(menuItem => {
-                        menuItem.classList.remove('active');
-                    });
-
-                    // Thêm hiệu ứng "active" cho mục tương ứng
+        if (rect.top >= 0 && rect.top <= 50) {
+                menuItems.forEach(menuItem => {
+                    menuItem.classList.remove('active');
+                    }
+                );
                     menuItems[i].classList.add('active');
                 }
             }
-        });
+        }
+        );
